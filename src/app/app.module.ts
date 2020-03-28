8e5d907458bac84b1c37031de29f414029e7e139
khanhhd0411 <kienkakax1@gmail.com>
2020-03-28 16:02:37 +0700

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ProductComponent } from './product/product.component';
-import { ProductListComponent } from './product-list/product-list.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent, ProductComponent, ProductListComponent ],
+  declarations: [ AppComponent, HelloComponent, ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
