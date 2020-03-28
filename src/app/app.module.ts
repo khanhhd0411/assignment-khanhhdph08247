995336b0f8c84879805a7416dc44a607fe5835c4
khanhhd0411 <kienkakax1@gmail.com>
2020-03-28 17:39:24 +0700

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ServiceComponent } from './service/service.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProductComponent, ProductListComponent, ],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, ProductComponent, ProductListComponent, ServiceComponent, ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
